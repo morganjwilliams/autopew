fd015a4ae8755589a3bbd01888684cecb994c854
Morgan Williams <morgan.j.williams@hotmail.com>
2019-03-15 09:33:25 +0800

    Update Image Registration

--- autopew/util/gui.py
+++ autopew/util/gui.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import logging
-from matplotlib.backends.backend_qt5 import TimerBase
-from matplotlib.backend_bases import MouseEvent
+from matplotlib.backend_bases import TimerBase
+from .plot import *
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 logger = logging.getLogger(__name__)
 
@@ -164,3 +164,37 @@ class ZoomPan(object):
 
         # return the function
         return onMotion
+
+def image_point_registration(img, timeout=None):
+    """
+    Launches a window which can be clicked to add points.
+    """
+    plt.ion()
+    fig, ax = plt.subplots()
+    points = []
+
+    def on_click(event):
+        if event.button == 1:
+            if event.inaxes is not None:
+                x, y = event.xdata, event.ydata
+                ax.scatter(x, y, marker="D", s=50, zorder=2)
+                ax.figure.canvas.draw()
+                points.append([x, y])
+            else:
+                print("Clicked ouside axes bounds but inside plot window")
+
+
+    fig.canvas.callbacks.connect("button_press_event", on_click)
+
+    zp = ZoomPan()
+    scale = 1.1
+    figZoom = zp.zoom_factory(ax, base_scale=scale)
+    figPan = zp.pan_factory(ax)
+    ax.imshow(img, origin='upper')
+    if timeout is not None:
+        _timeout(fig, timeout)
+        fig.timer.start()
+    plt.show(block=True)
+    plt.ioff()
+    points = np.array(points)
+    return points
