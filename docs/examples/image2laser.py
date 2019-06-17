9943bfdf5b62e5792d2abfce80ef052fd6a147d8
Morgan Williams <morgan.j.williams@hotmail.com>
2019-06-17 10:13:27 +0800

    Update Image2laser eg

--- docs/examples/image2laser.py
+++ docs/examples/image2laser.py
@@ -3,16 +3,18 @@
 from autopew.session import Session
 from pathlib import Path
 from autopew.util.writelase import xy2scansv
+from bs4 import UnicodeDammit
 
 s = Session()
-img = "NOR1-3B", Path("../../autopew/data/") / "images" / "NOR1-3B.jpg" # image
-newpoints = Path("../../autopew/data/") / "NOR1-3B.csv" #
-scancsv = Path("../../autopew/data/") / "_AutosavedScans.scancsv" # coordination points
+img = "NOR1-3B", Path("../../autopew/data/") / "images" / "NOR1-3B.jpg"  # image
+newpoints = Path("../../autopew/data/") / "NOR1-3B.csv"  #
+scancsv = Path("../../autopew/data/") / "_AutosavedScans.scancsv"  # coordination points
 
 newverts = s.autoflow(img, scancsv.resolve(), newpoints.resolve())
 
-df = xy2scansv(newverts, spotname_prefix='NOR1-3B')
+df = xy2scansv(newverts, spotname_prefix="NOR1-3B")
 
-with open('testfile.scansv', 'wb') as f:
-    str = df.to_csv(index=False).encode('utf-8')
+encoding = "cp1252"
+with open(Path("testfile").with_suffix(".scancsv"), "w", encoding=encoding) as f:
+    str = df.to_csv(index=False, encoding=encoding)
     f.write(str)
