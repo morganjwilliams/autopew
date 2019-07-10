528ad3c4e725e239744b29e23049a3da9bcbadf7
Morgan Williams <morgan.j.williams@hotmail.com>
2019-07-10 12:20:28 +0800

    Expand image tests

--- test/image/test_image.py
+++ test/image/test_image.py
@@ -4,8 +4,6 @@ from scipy import misc
 import matplotlib.pyplot as plt
 from autopew.image.base import PewImage, affine_extent, extent_to_size
 from autopew.transform.affine import zoom, rotate, translate, shear, affine_transform
-import PIL.Image
-
 
 class TestPewImage(unittest.TestCase):
     def setUp(self):
@@ -71,6 +69,35 @@ class TestPewImage(unittest.TestCase):
         r, g, b, c = im.maprgb()
         im = plt.pcolormesh(xi, yi, r, color=c)
         im.set_array(None)
+
+    def test_transforms_graphical(self):
+        im = PewImage(self.img)
+        A1, A2, A3 = zoom(1, 1.5), shear(0.3, 0), rotate(45)
+        _im1 = im.affine_transform(A1)
+        _im2 = im.affine_transform(A2)
+        _im3 = im.affine_transform(A3)
+
+        centre = np.array(im.image.size) / 2.0
+        fig, ax = plt.subplots(2, 4, figsize=(12, 3), sharex=True, sharey=True)
+        ax[0, 0].scatter(*centre, color="r")
+        # get the max extent of images
+        x0, x1, y0, y1 = 0, 1, 0, 1
+        for ix, I in enumerate([im, _im1, _im2, _im3]):
+            x0, x1, y0, y1 = (
+                min(x0, np.min(I.extent[:2])),
+                max(x1, np.max(I.extent[:2])),
+                min(y0, np.min(I.extent[2:])),
+                max(y1, np.max(I.extent[2:])),
+            )
+            ax[0, ix].imshow(I.image)
+            ax[1, ix].imshow(I.image, extent=I.extent)
+
+        size = np.max(im.image.size) * np.sqrt(2)
+        ax = ax.flat
+
+        for a in ax:
+            a.set_aspect("equal")
+            a.axis([x0, x1, y0, y1])
 
 
 if __name__ == "__main__":
