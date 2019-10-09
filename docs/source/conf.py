d2de7dd63e64a2d1be79dfb3a38115599a547931
Morgan Williams <morgan.j.williams@hotmail.com>
2019-10-09 10:02:07 +0800

    Update Docs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,7 +99,7 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {"prev_next_buttons_location": None}
+html_theme_options = {"prev_next_buttons_location": None, "vcs_pageview_mode": "edit"}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -120,6 +120,7 @@ html_sidebars = {
 
 html_context = {
     "display_github": True,  # Integrate GitHub
+    "last_updated": True,
     "github_user": "morganjwilliams",  # Username
     "github_repo": "autopew",  # Repo name
     "github_version": "develop",  # Version
